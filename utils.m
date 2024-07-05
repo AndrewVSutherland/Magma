d9c930cf66fa2b52a8e3fd4be70019f4ff2dd65b
Andrew V. Sutherland <drew@math.mit.edu>
2024-07-05 14:29:47 -0600

    add PolycyclicPresentation

--- utils.m
+++ utils.m
@@ -296,6 +296,27 @@ intrinsic Base26Decode(s::MonStgElt) -> RngIntElt
     return n;
 end intrinsic;
 
+// This implementation is slow and suitable only for small groups
+intrinsic PolycyclicPresentation(gens::SeqEnum,m::UserProgram,id::.) -> SeqEnum[RngIntElt], Map
+{ Given a sequence of generators in a uniquely represented hashable polycyclic group with composition law m and identity element id, returns sequence of relative orders and a map from group elements to exponent vectors.}
+    r := [Integers()|];
+    if #gens eq 0 then return r, func<a|r>; end if;
+    n := #gens;
+    T := [Universe(gens)|id];
+    while true do g := m(T[#T],gens[1]); if g eq id then break; end if; Append(~T,g); end while;
+    Append(~r,#T);
+    for i:=2 to n do
+        X := Set(T); S := T; j := 1;
+        g := gens[i];  h := g; while not h in X do S cat:= [m(t,h):t in T]; h := m(h,g); j +:= 1; end while;
+        Append(~r,j);  T := S;
+    end for;
+    ZZ := Integers();
+    A := AssociativeArray(Universe(gens));
+    for i:=1 to #T do A[T[i]] := i-1; end for;
+    rr := [ZZ|1] cat [&*r[1..i-1]:i in [2..n]];
+    return r, func<x|[Integers()|(e div rr[i]) mod r[i] : i in [1..n]] where e:=A[x]>;
+end intrinsic;
+
 intrinsic OrderStats(G::Grp) -> SetMulti[RngIntElt]
 { Multiset of order statistics of elements of the group G. }
     if #G eq 1 then return {*1*}; end if;
